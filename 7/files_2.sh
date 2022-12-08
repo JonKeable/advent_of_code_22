2b7ea4e645fe96af1476f874724a79ac345c813a
JonKeable <jekeable@live.co.uk>
2022-12-08 12:05:11 +0000

    got rid or print statements; copied sol1 for p2

---
+++ 7/files_2.sh
@@ -0,0 +1,59 @@
+#!/bin/awk -f
+BEGIN {
+    print "begin"
+    depth = 0
+    currDir = ""
+    TOTAL_SPACE = 70000000
+    SPACE_REQ = 30000000
+}
+
+{
+    if ($1 == "$") {
+        if ($2 == "cd") {
+            print "change directory"
+            ## i don't know why, but using == ".." didnt work! (or == anyting else, like "ee")
+            if ($3 ~ /\.\./) {
+                print "go up"
+                delete dirDepths[depth]
+                depth--
+                currDir = dirDepths[depth]
+            }
+            else {
+                # need this as some subdirs in different parent dirs have the same name, this was the source of issues
+                currDir = (currDir "/" $3)
+                dirDepths[depth] = currDir
+                depth++
+                files[currDir]+=0
+            }
+        }
+    }
+
+    else if ($1 == "dir") {
+
+    }
+
+    #files
+    else {
+
+        for (d in dirDepths) {
+            files[dirDepths[d]] += $1
+        }
+    }
+}
+
+
+END {
+
+
+    print "end stuff"
+
+
+    print "----------------"
+    sum = 0
+    for (dir in files) {
+
+    }
+
+    print ("sum : " sum)
+    print "end"
+}
