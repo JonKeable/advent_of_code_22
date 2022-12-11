99e9bf2d96b7036139832f7cf7f8c8839fc449c5
JonKeable <jekeable@live.co.uk>
2022-12-11 17:54:18 +0000

    part 1 solution

--- 11/monkey.py
+++ 11/monkey.py
@@ -3,8 +3,8 @@ from collections import deque
 class Monkey:
     def __init__ (self, id, startingItems, operation, test, t, f):
         self.id = int(id.split()[1].replace(':', ''))
-        self.startingItems = startingItems.split()[2:]
-        startingItems = [int(e.replace(',','')) for e in self.startingItems]
+        startingItems = startingItems.split()[2:]
+        startingItems = [int(e.replace(',','')) for e in startingItems]
         self.currentItems = deque()
         for e in startingItems :
             self.currentItems.append(e)
@@ -13,11 +13,35 @@ class Monkey:
         self.opX = opList[5]
         testList = test.split()
         self.testType = testList[1]
-        self.testX = testList[3]
+        self.testX = int(testList[3])
         self.trueMonkey = int(t.split()[-1])
         self.falseMonkey = int(f.split()[-1])
+        self.inspections = 0
 
     def __str__(self) -> str:
-        return("Monkey " + str(self.id) + ':')
+        return("Monkey " + str(self.id) + ': ' + str(self.currentItems) 
+        + "\n" + str(self.inspections) + " inspections")
 
+    def popItem(self):
+        return self.currentItems.popleft()
 
+    def addItem(self, item):
+        self.currentItems.append(item)
+
+    def inspect(self) :
+        self.inspections += 1
+        item = self.popItem()
+        opX = item if self.opX == 'old' else int(self.opX)
+        worryLevel = 0
+        if self.operand == '*':
+            worryLevel = item * opX
+        elif self.operand == '+':
+            worryLevel = item + opX
+        worryLevel  = worryLevel // 3
+        if worryLevel % self.testX == 0:
+            return (self.trueMonkey, worryLevel)
+        else:
+            return (self.falseMonkey, worryLevel)
+    
+
+    
--- 11/sim.py
+++ 11/sim.py
@@ -1,7 +1,14 @@
 from monkey import Monkey
 import numpy as np
 
-f = open("input.txt", 'r')
+
+def takeTurn(monkey, list):
+    while (monkey.currentItems):
+        result = monkey.inspect()
+        list[result[0]].addItem(result[1])
+
+
+f = open('input.txt', 'r')
 lineList = f.read().splitlines()
 lineList.append('')
 
@@ -17,7 +24,27 @@ for l in monkeyList:
 
 print(monkeyList)
 
-monkeyList = [Monkey(m[0], m[1], m[2],m[3],m[4],m[5]) for m in monkeyList]
+monkeyList = [Monkey(m[0], m[1], m[2], m[3], m[4], m[5]) for m in monkeyList]
 
-for monkey in monkeyList:
-    print (monkey)
+
+def printMonkeys(list):
+    for monkey in list:
+        print(monkey)
+
+
+numberOfRounds = 20
+for round in range(1, numberOfRounds+1):
+    for monkey in monkeyList:
+        takeTurn(monkey, monkeyList)
+
+printMonkeys(monkeyList)
+
+inspectionsCounts = sorted([monkey.inspections for monkey in monkeyList], reverse=True)
+
+print(inspectionsCounts)
+
+monkeyBusiness = inspectionsCounts[0] * inspectionsCounts[1]
+
+print(monkeyBusiness)
+
+
