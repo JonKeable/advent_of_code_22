de9e7761d04a033288e15e71ba57594ea16072e5
JonKeable <jekeable@live.co.uk>
2022-12-13 17:23:25 +0000

    intial ugly recursive partial solution

--- 13/packets.py
+++ 13/packets.py
@@ -7,6 +7,40 @@ def compare(pair):
     left = pair[0]
     right = pair[1]
 
+    leftIsInt = isinstance(left, int)
+    rightIsInt = isinstance(right, int)
+
+    if leftIsInt and rightIsInt:
+        valid = right - left
+        if valid > 0:
+            return True
+        elif valid < 0:
+            return False
+        else:
+            return None
+    
+    elif leftIsInt:
+        left = [left]
+    
+    elif rightIsInt:
+        right = [right]
+
+    lenLeft = len(left)
+    for i, val in enumerate(right):
+        if i < lenLeft:
+            valid = compare((left[i],val))
+            if valid != None:
+                return valid
+        else:
+            return True
+            
+    
+    if lenLeft > len(right):
+        return False
+    else:
+        return None
+
+
 
 f = open('test.txt', 'r')
 lineList = f.read().splitlines()
@@ -21,3 +55,10 @@ pairList = [arr.tolist()[:2] for arr in pairList]
 pairList = [(eval(pair[0]), eval(pair[1])) for pair in pairList]
 
 printLines(pairList)
+
+validList =[]
+
+for pair in pairList:
+    validList.append(compare(pair))
+
+print (validList)
